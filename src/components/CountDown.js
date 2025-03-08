1e64d8e21a2083d673406e5b91c8c0f393168d29
Pablo Rodríguez Sánchez <pabrodsan@alum.us.es>
2025-03-08 23:00:34 +0100

    fIX COUNTDOWN

--- src/components/CountDown.js
+++ src/components/CountDown.js
@@ -7,7 +7,7 @@ const CountDown = () => {
     const [minutes, setMinutes] = useState(0);
     const [seconds, setSeconds] = useState(0);
     useEffect(() => {
-        const countDownDate = new Date("Sept 13, 2025 22:53:35").getTime();
+        const countDownDate = new Date("Sept 13, 2025 14:30:00").getTime();
         const x = setInterval(() => {
 
             // Get today's date and time
