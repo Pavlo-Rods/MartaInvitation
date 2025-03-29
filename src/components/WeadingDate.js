247ffd1977c37b1228fc20a60bd25adb4b718177
Pablo Rodríguez Sánchez <pabrodsan@alum.us.es>
2025-03-29 23:26:05 +0100

    Change Initial screen

--- src/components/WeadingDate.js
+++ src/components/WeadingDate.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import boda from '../images/boda.png';
 import hearths from '../images/hearth.png';
+import flores from '../images/flores.png';
+import edificio from '../images/edificio.png';
 import '../WeadingDate.css';
 
 const WeadingDate = () => {
     return (
         <div className="main-slider">
+            <img src={flores} alt='flores' style={{
+                        width: '200vw',
+                        right: 1,
+                        zIndex: -1,
+                        position: 'absolute',
+                    }}/>
+            <img src={edificio} alt='edificio' style={{
+                position: 'absolute',
+                bottom: 0,
+                width: '150vw',
+                zIndex: -1,
+                left: 0,
+                height:  'auto'
+            }}/>
             <div className="display-table center-text">
                 <div className="display-table-cell">
+                    
                     <div className="slider-content">
 
                         <img className="sindur-dan" src={boda} alt="couple"/>
