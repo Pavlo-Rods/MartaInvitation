bdb83c71391354cded9a3c10aea43b37b5db5948
Pablo Rodríguez Sánchez <pabrodsan@alum.us.es>
2025-03-29 23:56:58 +0100

    Fix form

--- src/components/forms.js
+++ src/components/forms.js
@@ -1,5 +1,5 @@
-import React, {useState} from "react";
-import "../commoncss/GoogleForm.css";
+import React, { useState } from "react";
+import "../commoncss/form.css";
 
 const GoogleForm = () => {
   const [formData, setFormData] = useState({
@@ -7,35 +7,13 @@ const GoogleForm = () => {
     asistencia: "",
     asistentes: "",
     numAsistentes: "",
-    alergias: [],
-    otrasAlergias: "",
-  }); 
+    alergias: "",
+    bus: "",
+  });
 
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
-
-    // Si el usuario está escribiendo en "Otras Alergias", actualiza la lista de alergias
-    if (name === "otrasAlergias") {
-      setFormData((prev) => {
-        let nuevasAlergias = prev.alergias.filter((a) => !a.startsWith("Otros:")); // Borra versiones previas
-        if (value.trim() !== "") {
-          nuevasAlergias.push(`Otros: ${value}`);
-        }
-        return { ...prev, alergias: nuevasAlergias };
-      });
-    }
-  };
-
-  const handleCheckboxChange = (e) => {
-    const { value, checked } = e.target;
-    setFormData((prev) => {
-      let nuevasAlergias = checked
-        ? [...prev.alergias, value] // Añadir si está marcado
-        : prev.alergias.filter((alergia) => alergia !== value); // Quitar si se desmarca
-
-      return { ...prev, alergias: nuevasAlergias };
-    });
   };
 
   const handleSubmit = (e) => {
@@ -51,17 +29,19 @@ const GoogleForm = () => {
     var googleFormURL = "https://docs.google.com/forms/d/e/1FAIpQLScDyvhuXmQoXbgVn6KaBz3pTHQFHrqzpr7yWf1L3WHpzUY6UQ/formResponse";
 
     googleFormURL += "?entry.1092877632=" + formData.nombre;
-    googleFormURL += "&entry.1843499636=" + formData.asistencia; 
+    googleFormURL += "&entry.1843499636=" + formData.asistencia;
     googleFormURL += "&entry.715290076=" + formData.asistentes;
     googleFormURL += "&entry.914207409=" + formData.numAsistentes;
-    googleFormURL += "&entry.115438155=" + formData.alergias.join(", ");
+    googleFormURL += "&entry.115438155=" + formData.alergias;
+    googleFormURL += "&entry.452493007=" + formData.bus;
 
     const formBody = new URLSearchParams();
     formBody.append("entry.1092877632", formData.nombre);
     formBody.append("entry.1843499636", formData.asistencia);
     formBody.append("entry.715290076", formData.asistentes);
-    formBody.append("entry.115438155", formData.alergias.join(", "));
-
+    formBody.append("entry.914207409", formData.numAsistentes);
+    formBody.append("entry.115438155", formData.alergias);
+    formBody.append("entry.452493007", formData.bus);
 
     fetch(googleFormURL, {
       method: "POST",
@@ -70,57 +50,99 @@ const GoogleForm = () => {
       .then(() => alert("Gracias " + formData.nombre + " por confirmar tu asistencia"))
       .catch((error) => alert("Error al enviar el formulario"));
   };
+
   return (
     <div className="form-container">
       <div className="form-card">
-        <h2>Confirmanos tu asistencia</h2>
+        <h2>Confirmación de asistencia</h2>
         <form onSubmit={handleSubmit}>
           <div className="form-group">
             <label>Nombre:</label>
-            <input type="text" name="nombre" value={formData.nombre} onChange={handleChange} required />
+            <input 
+              type="text" 
+              name="nombre" 
+              value={formData.nombre} 
+              onChange={handleChange} 
+              placeholder="Escribe tu nombre completo"
+              required 
+            />
           </div>
 
           <div className="form-group">
             <label>¿Asistirás?</label>
-            <select name="asistencia" value={formData.asistencia} onChange={handleChange} required>
-              <option value="">Selecciona...</option>
+            <select 
+              name="asistencia" 
+              value={formData.asistencia} 
+              onChange={handleChange} 
+              required
+            >
+              <option value="">Selecciona una opción...</option>
               <option value="Si">Sí</option>
               <option value="No">No</option>
             </select>
           </div>
 
-          <div className="form-group">
-            <label>¿Vendrás acompañado?</label>
-            <select name="asistentes" value={formData.asistentes} onChange={handleChange} required={formData.asistencia === "Si"} // Conditionally set "required"
-              disabled={formData.asistencia === "No"} >
-              <option value="">Selecciona...</option>
-              <option value="Si">Sí</option>
-              <option value="No">No</option>
-            </select>
-            {formData.asistentes === "Si" && (
+          {formData.asistencia === "Si" && (
+            <>
               <div className="form-group">
-              <label>¿Por cuanta gente?</label>
-              <input type="number" placeholder="" name="numAsistentes" value={formData.numAsistentes} onChange={handleChange} required />
+                <label>¿Vendrás acompañado?</label>
+                <select 
+                  name="asistentes" 
+                  value={formData.asistentes} 
+                  onChange={handleChange} 
+                  required
+                >
+                  <option value="">Selecciona una opción...</option>
+                  <option value="Si">Sí</option>
+                  <option value="No">No</option>
+                </select>
               </div>
-            )}
+
+              {formData.asistentes === "Si" && (
+                <div className="form-group">
+                  <label>¿Cuántas personas te acompañarán?</label>
+                  <input 
+                    type="number" 
+                    name="numAsistentes" 
+                    value={formData.numAsistentes} 
+                    onChange={handleChange} 
+                    min="1"
+                    placeholder="Número de acompañantes"
+                    required 
+                  />
+                </div>
+              )}
+            </>
+          )}
+
+<div className="form-group">
+            <label>Necesitaras transporte en bus:</label>
+            <select 
+              name="bus" 
+              value={formData.bus} 
+              onChange={handleChange} 
+              required
+            >
+              <option value="">Selecciona una opción...</option>
+              <option value="No">No</option>
+              <option value="Solo ida">Solo ida</option>
+              <option value="Solo vuelta">Solo vuelta</option>
+              <option value="Ida y vuelta">Ida y vuelta</option>
+            </select>
           </div>
 
           <div className="form-group">
-            <label>Alergias:</label>
-            <div className="checkbox-group">
-              {["Frutos secos", "Lactosa", "Gluten", "Otros"].map((alergia) => (
-                <label key={alergia}>
-                  <input type="checkbox" value={alergia} onChange={handleCheckboxChange} />
-                  {alergia}
-                </label>
-              ))}
-            </div>
-            {formData.alergias.includes("Otros") && (
-              <input type="text" name="otrasAlergias" placeholder="Especificar otras alergias" value={formData.otrasAlergias} onChange={handleChange} />
-            )}
+            <label>Alergias o restricciones alimentarias:</label>
+            <textarea 
+              name="alergias" 
+              value={formData.alergias} 
+              onChange={handleChange} 
+              placeholder="Si tienes alguna alergia o restricción alimentaria, por favor especifícala aquí"
+              rows="3"
+            />
           </div>
 
-          <button type="submit">Confirmar Asistencia</button>
+          <button type="submit" className="submit-button">Confirmar Asistencia</button>
         </form>
       </div>
     </div>
