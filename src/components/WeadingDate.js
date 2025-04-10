a4af49f425110de83750a7c81cecdf93e1755d0b
Pablo Rodríguez Sánchez <pabrodsan@alum.us.es>
2025-04-10 18:42:09 +0200

    Cambios en slider y color

--- src/components/WeadingDate.js
+++ src/components/WeadingDate.js
@@ -8,25 +8,25 @@ import "@fontsource/pinyon-script";
 
 const WeadingDate = () => {
     return (
-        <div className="main-slider">
+        <div className="main-slider" style={{backgroundColor:'white', color:'white'}}>
             <img src={flores} alt='flores' style={{
                         width: '200vw',
                         right: 1,
-                        zIndex: -1,
+                        zIndex: 1,
                         position: 'absolute',
                     }}/>
             <img src={edificio} alt='edificio' style={{
                 position: 'absolute',
                 bottom: 0,
                 width: '150vw',
-                zIndex: -1,
+                zIndex: 1,
                 left: 0,
                 height:  'auto'
             }}/>
-            <div className="display-table center-text">
-                <div className="display-table-cell">
+            <div className="display-table center-text" style={{backgroundColor:'white', color:'white'}}>
+                <div className="display-table-cell" style={{backgroundColor:'white', color:'white'}}>
                     
-                    <div className="slider-content">
+                    <div className="slider-content" style={{backgroundColor:'white'}}>
 
                         <h5 className="date" style={{fontFamily: 'Pinyon Script', color: '#5e0416'}}>13 de Septiembre del 2025</h5>
                         <h1 className="title" style={{fontFamily: 'Pinyon Script', color: '#5e0416'}}>Marta   <img className="sindur-dan" src={hearths} alt="hearths"/> Jaime</h1>
@@ -36,6 +36,8 @@ const WeadingDate = () => {
                             <h3 className="pre-title" style={{fontSize: '6vw', fontFamily: 'Pinyon Script', color: '#5e0416', textAlign:'left',marginLeft: '2vw'}}>Aunque no le guste bailar,</h3>
                             <h3 className="pre-title" style={{fontSize: '6vw', fontFamily: 'Pinyon Script', color: '#5e0416', textAlign:'left', marginLeft: '2vw'}}>Y lo haga porque es contigo y nada mas</h3>
                             <h3 className="pre-title" style={{fontSize: '5vw', fontFamily: 'Pinyon Script', color: '#5e0416', textAlign:'right', marginRight: '2vw'}}>~ Jorge Luis Borges</h3>
+
+                            <h4 style={{fontSize: '4vw', fontFamily: 'Pinyon Script', color: '#5e0416', marginTop: '2vh'}}>ES05 2100 8436 4702 0114 7428</h4>
 
                     </div>
 			</div>
